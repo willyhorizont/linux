005c02011c244f184a137bfb7835ac5dbc1fe626
willyhorizont <willyhorizont@gmail.com>
2026-09-02 03:16:23 +0700

    [Last updated: 02 Sep 2026 @ 03:16 AM][version: 0.0.2] update lemonbar-simple-resource-monitor.sh; update yabar-simple-resource-monitor.sh;

--- tools/last-commit.sh
+++ tools/last-commit.sh
@@ -2,7 +2,7 @@
 
 SD=$(dirname "$(realpath "$0")")
 RD=$(realpath "$SD/..")
-V="0.0.1" # ! DON'T FORGET TO CHANGE VERSION BEFORE RUNNING !!!!
+V="0.0.2" # ! DON'T FORGET TO CHANGE VERSION BEFORE RUNNING !!!!
 T=$(date "+%d %b %Y @ %I:%M %p")
 cd "$RD" || exit
 
@@ -12,7 +12,8 @@ H="
 H=$(sed -e '/./,$!d' <<< "$H")
 # ! DON'T FORGET TO CHANGE COMMIT MESSAGE BEFORE RUNNING !!!!
 M="
-first commit :);
+update lemonbar-simple-resource-monitor.sh;
+update yabar-simple-resource-monitor.sh;
 "
 M=$(sed -e '/./,$!d' <<< "$M")
 M="$H
