d4af67b15b03d86d899ec4b68bc652c9f17c6955
willyhorizont <willyhorizont@gmail.com>
2026-09-04 16:09:25 +0700

    [Last updated: 04 Sep 2026 @ 04:09 PM][version: 0.0.7] remove linux minx IceWM; remove mindora linux Labwc;

--- tools/last-commit.sh
+++ tools/last-commit.sh
@@ -2,7 +2,7 @@
 
 SD=$(dirname "$(realpath "$0")")
 RD=$(realpath "$SD/..")
-V="0.0.6" # ! DON'T FORGET TO CHANGE VERSION BEFORE RUNNING !!!!
+V="0.0.7" # ! DON'T FORGET TO CHANGE VERSION BEFORE RUNNING !!!!
 T=$(date "+%d %b %Y @ %I:%M %p")
 cd "$RD" || exit
 
@@ -24,5 +24,5 @@ git add .
 git commit -m "$M"
 git tag -d "$V" 2>/dev/null
 git tag -a "$V" -m "$M"
-git push origin main
-git push origin --tags
+git push origin main -f
+git push origin --tags -f
